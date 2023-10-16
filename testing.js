ea56271ba1e2a66e16a2f2a4fc16c2e46f83e3c5
Maxim Toma <maxim.toma@sky.uk>
2023-10-16 12:51:57 +0100

    New JS file

---
+++ testing.js
@@ -0,0 +1,10 @@
+function print(text) {
+  console.log(text);
+}
+
+function helloThere(name) {
+  // Example: Hello there <name>, how are you?
+  return "Solve this";
+}
+
+print(helloThere("Maxim"));
